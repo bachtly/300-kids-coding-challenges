f29ec4e54bab348adc8b8251dca4d567032fe4d0
lybach <ltb1002.edmail@gmail.com>
2024-07-25 21:20:49 +0700

    optimize by: 1) remove recursion by interation 2) optimize the "stack" by only adding valid values to stack (see update() method)

--- RangeQueries/hotel_queries.py
+++ RangeQueries/hotel_queries.py
@@ -1,17 +1,14 @@
-class SegmentTree:
-    def __init__(self, arr, combine, default_value):
+class SegmentTreeMax:
+    def __init__(self, arr, default_value):
         """
         Parameters
         ----------
         arr :
            The base array
-        combine :
-           Method to aggregate two nodes (e.g. max, sum)
         """
 
         self.n = len(arr)
         self.tree = [0] * (4 * len(arr))
-        self.combine = combine
         self.default_value = default_value
 
         for idx, num in enumerate(arr):
@@ -19,71 +16,70 @@ class SegmentTree:
 
     def update(self, position, value):
         # print(locals())
-        self._update(1, 1, self.n, position, value)
-        # self.print_prefix_sum()
 
-    def _update(self, idx, left, right, position, value):
-        # print(locals())
+        # remove recursion by a stack (idx, left, right)
+        stack = [(1, 1, self.n)]
+        while stack:
+            if isinstance(stack[-1], tuple) and len(stack[-1]) == 3:
+                idx, left, right = stack.pop()
 
-        if not left <= position <= right:
-            return
+                if left == right:
+                    self.tree[idx] = value
+                    continue
 
-        if left == right:
-            self.tree[idx] = value
-            return
+                else:
+                    # when see integer in stack, it is the tree node, update that node
+                    stack.append(idx)
+                    mid = (left + right) // 2
+                    if left <= position <= mid:
+                        stack.append((idx * 2, left, mid))
+                    if mid + 1 <= position <= right:
+                        stack.append((idx * 2 + 1, mid + 1, right))
+            else:
+                tree_idx = stack.pop()
+                self.tree[tree_idx] = max(self.tree[tree_idx * 2], self.tree[tree_idx * 2 + 1])
 
-        mid = (left + right) // 2
-        self._update(idx * 2, left, mid, position, value)
-        self._update(idx * 2 + 1, mid + 1, right, position, value)
-        self.tree[idx] = self.combine(self.tree[idx * 2], self.tree[idx * 2 + 1])
+    def max(self):
+        return self.tree[1]
 
-    def get(self, left, right):
-        return self._get(1, 1, self.n, left, right)
+    def left_most_ge(self, value):
+        if value > self.tree[1]:
+            return 0
 
-    def _get(self, idx, left, right, query_left, query_right):
-        # print(locals())
-        if query_left <= left and right <= query_right:
-            return self.tree[idx]
+        # remove recursion by a stack (idx, left, right)
+        stack = [(1, 1, self.n)]
+        while stack:
+            idx, left, right = stack.pop()
 
-        if query_left > right or query_right < left:
-            return self.default_value
+            if left == right:
+                return left
 
-        mid = (left + right) // 2
-        left_branch = self._get(idx * 2, left, mid, query_left, query_right)
-        right_branch = self._get(idx * 2 + 1, mid + 1, right, query_left, query_right)
-        return self.combine(left_branch, right_branch)
+            mid = (left + right) // 2
+            if self.tree[idx * 2] >= value:
+                stack.append((idx * 2, left, mid))
+                continue
+            else:
+                stack.append((idx * 2 + 1, mid + 1, right))
+                continue
 
-    def print_prefix_sum(self):
-        for i in range(1, self.n + 1):
-            print(self.get(1, i), end=' ')
-        print()
+        return self.default_value
 
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
     hotels = [int(i) for i in input().split()]
 
-    tree = SegmentTree(hotels, max, 0)
+    tree = SegmentTreeMax(hotels, 0)
 
     groups = map(int, input().split())
     for group in groups:
-        if group > tree.get(1, n):
+        if group > tree.max():
             print('0', end=' ')
             continue
 
-        left = 1
-        right = n
-        while left < right:
-            mid = (left + right) // 2
-            max_prefix = tree.get(1, mid)
-            if max_prefix >= group:
-                right = mid
-            else:
-                left = mid + 1
-
         # now left is first matched
-        hotels[left - 1] -= group
-        tree.update(left, hotels[left - 1])
+        left_most_ge = tree.left_most_ge(group)
+        hotels[left_most_ge - 1] -= group
+        tree.update(left_most_ge, hotels[left_most_ge - 1])
 
-        # tree.print_prefix_sum()
-        print(left, end=' ')
+        print(left_most_ge, end=' ')
