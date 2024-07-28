d887f2701aead2fc18e766ce64d84bcb8ad7e29c
lybach <ltb1002.edmail@gmail.com>
2024-07-28 12:58:46 +0700

    subordinates optimize by eliminating recursion by looping! GOT IT! NO LONGER TLE

--- TreeAlgorithms/subordinates.py
+++ TreeAlgorithms/subordinates.py
@@ -1,10 +1,29 @@
-def visit(node, subordinates):
-    cnt = 0
-    for child in adjacent.get(node, []):
-        visit(child, subordinates)
-        cnt += subordinates[child - 1] + 1
+class OperationUpdateNode:
+    def __init__(self, node, subordinates, adjacent):
+        self.node = node
+        self.subordinates = subordinates
+        self.adjacent = adjacent
 
-    subordinates[node - 1] = cnt
+    def __call__(self):
+        children = self.adjacent.get(self.node, [])
+        self.subordinates[self.node] = sum([subordinates[i] for i in children]) + len(children)
+        # print("update", locals())
+
+
+class OperationVisitNode:
+    def __init__(self, stack, node, subordinates, adjacent):
+        self.stack = stack
+        self.node = node
+        self.subordinates = subordinates
+        self.adjacent = adjacent
+
+    def __call__(self):
+        children = self.adjacent.get(self.node, [])
+        self.stack.append(OperationUpdateNode(self.node, self.subordinates, self.adjacent))
+        for child in children:
+            if child in adjacent:
+                self.stack.append(OperationVisitNode(self.stack, child, self.subordinates, self.adjacent))
+        # print("visit", locals())
 
 
 if __name__ == "__main__":
@@ -15,13 +34,20 @@ if __name__ == "__main__":
     top_managers = [1]
     for i, manager in enumerate(managers):
         if manager != 0:
-            adjacent[manager] = adjacent.get(manager, []) + [i + 2]
+            if manager in adjacent:
+                adjacent[manager].append(i + 2)
+            else:
+                adjacent[manager] = [i + 2]
         else:
             top_managers.append(i + 2)
 
-    subordinates = [0] * n
+    subordinates = [0] * (n + 1)
+    stack = []
+    for top_manager in top_managers:
+        stack.append(OperationVisitNode(stack, top_manager, subordinates, adjacent))
 
-    for i in top_managers:
-        visit(i, subordinates)
+    while stack:
+        stack_item = stack.pop()
+        stack_item()
 
-    print(' '.join(map(str, subordinates)))
+    print(' '.join(map(str, subordinates[1:])))
